56292fe551693c1776a05ed302eaeb9962050fbf
Amir Meimari <amirmeimari@gmail.com>
2020-12-18 17:47:23 +0330

    install axios - add fetch methods for users - add local data

--- src/store/index.js
+++ src/store/index.js
@@ -6,18 +6,46 @@ const initState = {
 
 // Types
 const UPDATE_USERS_LIST = 'UPDATE_USERS_LIST'
+const UPDATE_LOGGED_USER = 'UPDATE_LOGGED_USER'
 
 // Actions
 export const updateUsersList = (usersList) => {
-  return { type: UPDATE_USERS_LIST, usersList }
+  let localUsers = JSON.parse(localStorage.getItem('users'))
+  if (localUsers) {
+    // some users are already in local storage
+    // check if passedUsers are in local, if not update local
+    usersList.forEach((user) => {
+      if (!localUsers.some((lu) => lu.id === user.id)) {
+        // add to local
+        localUsers.push(user)
+      }
+    })
+
+    // update local storage
+    localStorage.setItem('users', JSON.stringify(localUsers))
+  } else {
+    // create users in local storage
+    localUsers = [...usersList]
+    localStorage.setItem('users', JSON.stringify(usersList))
+  }
+
+  return { type: UPDATE_USERS_LIST, localUsers }
 }
 
-// Reducers
+export const updateLoggedUser = (loggedUser) => {
+  // update local storage
+  localStorage.setItem('loggedUser', JSON.stringify(loggedUser))
+
+  return { type: UPDATE_LOGGED_USER, loggedUser }
+}
+
 // Reducers
 const rootReducer = (state = initState, action) => {
   switch (action.type) {
     case UPDATE_USERS_LIST:
       return { ...state, users: action.usersList }
+    case UPDATE_LOGGED_USER:
+      return { ...state, users: action.loggedUser }
     default:
       return state
   }
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,3 +1,11 @@
+import { useEffect } from 'react'
+import axios from 'axios'
+import { connect } from 'react-redux'
+import { updateUsersList, updateLoggedUser } from '../store/index'
+
+// local data
+import usersList from '../data/users.json'
+
 import Layout from '../components/Layout/Layout'
 import Header from '../components/Header/Header'
 import NewTweet from '../components/NewTweet/NewTweet'
@@ -5,7 +13,31 @@ import Divider from '../components/Divider/Divider'
 import Tweet from '../components/Tweet/Tweet'
 import Modal from '../components/Modal/Modal'
 
-const Home = () => {
+const Home = ({ dispatch }) => {
+  const BASE_URL = process.env.REACT_APP_BASE_API_ADDRESS
+
+  useEffect(() => {
+    // fetch users list from API
+    fetchUsersList()
+    handleUpdateLoggedUser()
+  }, [])
+
+  const fetchUsersList = async () => {
+    // fake fetch request
+    await axios.get(`${BASE_URL}/users/`)
+    // update users list with local data
+    dispatch(updateUsersList(usersList))
+    try {
+    } catch (e) {
+      console.log(`An error occurred white fetching users list`, e)
+    }
+  }
+
+  const handleUpdateLoggedUser = () => {
+    // in real cases logged user should be fetch and authenticated even before app loaded, for example after login, but here it's not the case so i will not send request, just update local storage and store
+    dispatch(updateLoggedUser(usersList[0]))
+  }
+
   const tweet = {
     id: 1,
     name: 'amir',
@@ -16,20 +48,23 @@ const Home = () => {
   }
 
   const handleSetNewTweetText = (v) => {
-    console.log(v);
+    console.log(v)
   }
 
   const handleSubmitNewTweet = () => {
-    console.log('wdawd');
+    console.log('wdawd')
   }
 
   return (
     <>
       <Layout>
         <Header title="Home" />
-        <NewTweet text={handleSetNewTweetText} onSubmit={() => handleSubmitNewTweet()} />
+        <NewTweet
+          text={handleSetNewTweetText}
+          onSubmit={() => handleSubmitNewTweet()}
+        />
         <Divider big />
-        <Tweet content={tweet}  />
+        <Tweet content={tweet} />
 
         <Modal title="Reply">
           <Tweet reply />
@@ -40,4 +75,10 @@ const Home = () => {
   )
 }
 
-export default Home
+const mapStateToPros = (state) => {
+  return {
+    tweets: state.tweets,
+  }
+}
+
+export default connect(mapStateToPros)(Home)
